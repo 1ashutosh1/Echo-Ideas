78beba0354cab591798694df795bf74bb38ff78b
1Ashutosh1 <at8969927@gmail.com>
2024-04-25 21:59:49 +0530

    Created Header Section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import DashBoard from './pages/DashBoard';
 import Projects from './pages/Projects';
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
