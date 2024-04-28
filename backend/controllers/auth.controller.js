fa4d72adf267c996e67607b54654df46c617f14d
1Ashutosh1 <at8969927@gmail.com>
2024-04-28 22:02:00 +0530

    Handling errors

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,10 +1,11 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (!username || !email || !password) {
-    return res.status(400).json({ message: "All Fields are required" });
+    next(errorHandler(400,'All fields are required'));
   }
 
   const hashedPassword = bcryptjs.hashSync(password,10);
@@ -19,6 +20,6 @@ export const signup = async (req, res) => {
     await newUser.save();
     res.json("Signup Successful");
   } catch(err){
-    res.status(500).json({message: err.message});
+    next(err); 
   }
 };
--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,16 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
+app.use((err,req,res,next) => {
+   const statusCode = err.statusCode || 500;
+   const message = err.message || 'Internal Server Error';
+   res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+   })
+})
+
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
