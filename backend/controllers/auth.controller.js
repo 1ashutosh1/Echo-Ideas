cf4f6871e5cb6489e793547ecfe9417f5ae80df8
1Ashutosh1 <at8969927@gmail.com>
2024-05-01 09:42:29 +0530

    complete signin page functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -21,5 +22,34 @@ export const signup = async (req, res, next) => {
     res.json("Signup Successful");
   } catch(err){
     next(err); 
+  }
+};
+
+
+export const signin = async(req,res,next) => {
+  const {email,password} = req.body;
+  if(!email || !password){
+    next(errorHandler(400,'All Fields are required'));
+  }
+
+  try {
+    const validUser = await User.findOne({email});
+    if(!validUser){
+      return next(errorHandler(404,'User Not Found'));
+    }
+
+    const validPassword = bcryptjs.compareSync(password,validUser.password);
+    if(!validPassword){
+      return next(errorHandler(400,'Invalid password')); 
+    }
+
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+    const {password: pass, ...rest} = validUser._doc;
+
+    res.status(200).cookie('access_token',token,{
+      httpOnly: true,
+    }).json(rest);
+  } catch (error) {
+    return next(error);
   }
 };
