01488f073b7f5e0c1b90b0df625be445c191fc4f
1Ashutosh1 <at8969927@gmail.com>
2024-05-04 01:40:05 +0530

    Created DashBoard Sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,29 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import DashBoard from './pages/DashBoard';
-import Projects from './pages/Projects';
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import DashBoard from "./pages/DashBoard";
+import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<DashBoard />}/>
-        <Route path="/projects" element={<Projects />}/>
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<DashBoard />} />
+        </Route>
+        <Route path="/projects" element={<Projects />} />
       </Routes>
-      <Footer /> 
+      <Footer />
     </BrowserRouter>
   );
 };
--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSideBar from "../components/DashSideBar";
+import DashProfile from '../components/DashProfile'
 
 const DashBoard = () => {
+  const location = useLocation();
+  const [tab,setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if(tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  },[location.search])
   return (
-    <div>DashBoard</div>
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+       {/* SideBar */}
+       <DashSideBar/>
+      </div>
+      {/* Profile */}
+      {tab === 'profile' && <DashProfile />}
+    </div>
   )
 }
 
-export default DashBoard
+export default DashBoard;
