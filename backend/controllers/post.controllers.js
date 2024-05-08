ed5164138558c1844d20689a0f78407bebe0c387
1Ashutosh1 <at8969927@gmail.com>
2024-05-09 00:22:35 +0530

    complete post page functionality

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -1,48 +1,54 @@
 import Post from "../models/post.model.js";
-import { errorHandler } from "../utils/error.js"
+import { errorHandler } from "../utils/error.js";
 
-export const create = async (req,res,next) => {
-   if(!req.user.isAdmin){
-    return next(errorHandler(403, 'You are not allowed to create a post'));
-   }
-   if(!req.body.title || !req.body.content){
-    return next(errorHandler(400, 'Please provide all required fields'));
-   }
+export const create = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to create a post"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please provide all required fields"));
+  }
 
-   const slug = req.body.title.toLowerCase().split(' ').join('-').replace(/[^a-zA-Z0-9-]/g, '');
+  const slug = req.body.title
+    .toLowerCase()
+    .split(" ")
+    .join("-")
+    .replace(/[^a-zA-Z0-9-]/g, "");
 
-   const newPost = new Post({
+  const newPost = new Post({
     ...req.body,
     slug,
-    userId: req.user.id
-   });
+    userId: req.user.id,
+  });
 
-   try {
+  try {
     const savedPost = await newPost.save();
     res.status(201).json(savedPost);
-   } catch (error) {
-    next(error)
-   }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getPosts = async(req,res,next) => {
-   try {
-      const startIndex = parseInt(req.query.startIndex) || 0;
-      const limit = parseInt(req.query.limit) || 9;
-      const sortDirection = req.query.order === 'asc' ? 1 : -1;
-      const posts = await Post.find({
-         ...(req.query.userId && {userId: req.query.userId}),
-         ...(req.query.category && {category: req.query.category}),
-         ...(req.query.slug && {slug: req.query.slug}),
-         ...(req.query.postId && {_id: req.query.postId}),
-         ...(req.query.postId && {_id: req.query.postId}),
-         ...(req.query.searchTerm && {
-            $or: [
-               {title: {$regex: req.query.searchTerm, $options: 'i'}},
-               {content: {$regex: req.query.searchTerm, $options: 'i'}},
-            ],
-         })
-   }).sort({updatedAt: sortDirection}).skip(startIndex).limit(limit);
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
 
     const totalPosts = await Post.countDocuments();
     const now = new Date();
@@ -53,7 +59,7 @@ export const getPosts = async(req,res,next) => {
     );
 
     const lastMonthPosts = await Post.countDocuments({
-      createdAt: {$gte: oneMonthAgo},
+      createdAt: { $gte: oneMonthAgo },
     });
 
     res.status(200).json({
@@ -61,40 +67,42 @@ export const getPosts = async(req,res,next) => {
       totalPosts,
       lastMonthPosts,
     });
-   } catch (error) {
-      next(error);
-   }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deletepost = async(req,res,next) => {
-   if(!req.user.isAdmin || req.user.id !== req.params.userId){
-      return next(errorHandler(403, 'You are not allowed to delete this post'));
-   }
-   try {
-      await Post.findByIdAndDelete(req.params.postId);
-      res.status(200).json('The post has been deleted');
-   } catch (error) {
-      next(error);
-   }
-}
+export const deletepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this post"));
+  }
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.status(200).json("The post has been deleted");
+  } catch (error) {
+    next(error);
+  }
+};
 
-
-export const updatepost = async(req,res,next) => {
-   if(!req.user.isAdmin || req.user.id !== req.params.userId){
-      return next(errorHandler(403, 'You are not allowed to update this post'));
-   }
-   try {
-      const updatedPost = await Post.findByIdAndUpdate(
-         req.params.postId,
-         {
-           $set:{
-            title: req.body.title,
-            content: req.body.content,
-            category: req.body.category,
-            image: req.body.image,
-           }}, {new: true})
-      res.status(200).json(updatedPost);
-   } catch (error) {
-      next(error);
-   }
-}
+export const updatepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this post"));
+  }
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        $set: {
+          title: req.body.title,
+          content: req.body.content,
+          category: req.body.category,
+          image: req.body.image,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
 
 const App = () => {
   return (
@@ -29,6 +30,7 @@ const App = () => {
           <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
         <Route path="/projects" element={<Projects />} />
+        <Route path="/post/:postSlug" element={<PostPage/>} />
       </Routes>
       <Footer />
     </BrowserRouter>
