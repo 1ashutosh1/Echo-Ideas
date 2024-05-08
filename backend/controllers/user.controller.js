f314761eb91a0e17bb630a0e17d5afd22bef0828
1Ashutosh1 <at8969927@gmail.com>
2024-05-08 17:02:20 +0530

    delete user functionality to admin dashboard

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -62,7 +62,7 @@ export const updateUser = async (req, res, next) => {
 
 
 export const deleteUser = async(req,res,next) => {
-   if(req.user.id !== req.params.userId){
+   if(!req.user.isAdmin && req.user.id !== req.params.userId){
      return next(errorHandler(403, 'You are not allowed to delete this account'));
    }
 
@@ -116,7 +116,7 @@ export const getUsers = async(req,res,next) => {
       totalUsers,
       lastMonthUsers,
     });
-    
+
   } catch (error) {
     next(error);
   }
