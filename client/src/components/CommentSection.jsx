1e5d8d2aa11ced776c0413527a5c82910702c0b9
1Ashutosh1 <at8969927@gmail.com>
2024-05-10 10:41:24 +0530

    edit comment functionality

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link,useNavigate } from "react-router-dom";
 import Comment from "./Comment";
+// eslint-disable-next-line react/prop-types
 export default function CommentSection({ postId }) {
   const { currentUser } = useSelector((state) => state.user);
   const [commentError, setCommentError] = useState(null);
@@ -80,6 +81,10 @@ export default function CommentSection({ postId }) {
       console.log(error.message);
     }
   };
+
+  const handleEdit = async(comment,editedContent) => {
+    setComments(comments.map((c) => c._id === comment._id ? {...c,content: editedContent} : c));
+  };
   return (
     <div className="max-w-2xl mx-auto w-full p-3">
       {currentUser ? (
@@ -145,7 +150,7 @@ export default function CommentSection({ postId }) {
        </div>
        {
        comments.map((comment) => (
-        <Comment key={comment._id} comment={comment} onLike={handleLike}/>
+        <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit}/>
        ))
        }
         </>
